0a24147e870b30669f5ddbac098b6ccae6bca2bd
MorbusXD <121089273+MorbusXD@users.noreply.github.com>
2022-12-22 18:09:28 +0800

    Hemu Bday

---
+++ homepage.js
@@ -0,0 +1,27 @@
+function checkCode(){
+    var secret = document.getElementById('code').value
+    if (secret == 'siiuo'){
+        window.location = 'seghals.html'
+    }
+    if (secret == 'tyyue') {
+        window.location = 'thakurs.html'
+    }
+    if (secret == 'loopi') {
+        window.location = 'ravikants.html'
+    }
+    if (secret == 'skiopp'){
+        window.location = 'shastrys.html'
+    }
+    if (secret == 'bjjio'){
+        window.location = 'bawas.html'
+    }
+    if (secret == 'siiopu'){
+        window.location = 'soods.html'
+    }
+    if (secret == 'blaps'){
+        window.location = 'Banavalikars.html'
+    }
+    if (secret == 'gulati'){
+        window.location = 'gulati.html'
+    }
+}
